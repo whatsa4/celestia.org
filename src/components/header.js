48d2890e47e9dca05dcbc28cb56993b236cdf10b
Gabor Sera <gabor@designatives.com>
2022-01-26 14:14:49 +0100

    change main menu

--- src/components/header.js
+++ src/components/header.js
@@ -21,14 +21,14 @@ const navigation = [
                 text: "Experiment with devnet",
                 subtext: "Devnet",
                 url: "https://github.com/celestiaorg/networks",
-                icon: "menu/specification.svg",
+                icon: "menu/devnet.svg",
                 type: 'external'
             },
             {
                 text: "Read the original research paper",
                 subtext: "Research paper",
                 url: "https://arxiv.org/pdf/1905.09274.pdf",
-                icon: "menu/paper.svg",
+                icon: "menu/research.svg",
                 type: 'external'
             }
         ]
@@ -76,7 +76,23 @@ const navigation = [
     },{
         text: "Resources",
         url: "/resources/",
-        type: 'internal'
+        type: 'internal',
+        submenus: [
+            {
+                text: "FAQ",
+                subtext: "Start here",
+                url: "/faq",
+                icon: "menu/faq.svg",
+                type: 'internal'
+            },
+            {
+                text: "Videos, Podcasts, Whitepapers",
+                subtext: "Resources",
+                url: "/resources",
+                icon: "menu/resources.svg",
+                type: 'internal'
+            }
+        ]
     },{
         text: "Blog",
         url: "https://blog.celestia.org",
