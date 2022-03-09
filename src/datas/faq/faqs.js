faa9efb46240328e1d6969eb46bf3958290f28d1
Alex Beckett <75361908+alexbeckettt@users.noreply.github.com>
2022-03-09 19:11:03 +0100

    Update faqs.js

--- src/datas/faq/faqs.js
+++ src/datas/faq/faqs.js
@@ -2,7 +2,7 @@ export const faqs = [
     {
         id: 1,
         question: 'What is Celestia?',
-        text: 'Celestia is the first modular blockchain network that makes it effortless to deploy a new blockchain. It allows sovereign blockchains to break free from the constraints of monolithic architectures so that they can build with flexibility and freedom on their own terms.<br><br>Celestia is a unique layer 1 blockchains whose only job is to order transactions and verify that the data being published is available. This allows specialized blockchain for hosting applications to deploy on top of it. By taking this approach, Celestia achieves properties of scalability, flexibility and interoperability unmatched by previous blockchain designs.',
+        text: 'Celestia is the first modular blockchain network that makes it effortless to deploy a new blockchain. It allows sovereign blockchains to break free from the constraints of monolithic architectures so that they can build with flexibility and freedom on their own terms.<br><br>Celestia is a unique layer 1 blockchain whose only job is to order transactions and verify that the published data is available. This allows specialized blockchains for hosting applications to deploy on top of it. By taking this approach, Celestia achieves properties of scalability, flexibility and interoperability unmatched by previous blockchain designs.',
     },{
         id: 2,
         question: 'What stage of development is Celestia in?',
