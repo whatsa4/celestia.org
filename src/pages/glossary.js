a23dfa6f1ed07951b74532f0d5bfe2efb61e238a
Gabor Sera <gabor@designatives.com>
2022-04-07 10:59:25 +0200

    glossary page fix

--- src/pages/glossary.js
+++ src/pages/glossary.js
@@ -13,12 +13,35 @@ class Result extends React.Component {
         return (
             <li className={'row result'}>
                 <Link to={`/glossary/${this.props.result.url}`} state={{url: this.props.result.url}}>
-                        <div className={'col-12'}>
-                            <div className={'title'}>{this.props.result.title}</div>
-                            {this.props.result.text}
-                        </div>
+                    <div className={'col-12'}>
+                        <div className={'title'}>{this.props.result.title}</div>
+                        {this.props.result.text}
+                    </div>
                 </Link>
             </li>
+        )
+    }
+}
+
+class Results extends React.Component {
+    render() {
+        return (
+            <div className={'row alpha-row'} key={Object.keys(this.props.glossary)}>
+                <div className={'col-12'}>
+                    <div className={'row'}>
+                        <div className={'col-12 col-md-2'}>
+                            <div className={'alpha'}>{Object.keys(this.props.glossary)}</div>
+                        </div>
+                        <div className={'col-12 col-md-10'}>
+                            <ul className={'result-list'}>
+                                {Object.values(this.props.glossary)[0].map((result,glossaryIndex,array) => (
+                                    <Result key={result.title} result={result} group={this.props.index} index={glossaryIndex}/>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
@@ -31,7 +54,6 @@ function SearchGlossary(e){
 
     let count = 0;
 
-
     glossaries.map(glossary => (Object.values(glossary)[0]
         .filter(v => v.title.toLowerCase().includes(string) || v.text.toLowerCase().includes(string))
         .reduce((key, obj) => {
@@ -40,23 +62,21 @@ function SearchGlossary(e){
         }, {})));
 
     return filteredGlossaries;
-
 }
 
 function GetGlossaries(props){
 
     const [search, toggleSearch] = useState(false)
+    const [filter, toggleFilter] = useState(false)
     const [expand, toggleExpand] = useState(false)
     const [filteredGlossaries, setGlossaries] = useState([])
-
-    console.log(props.alpha)
 
     return (
         <div>
             <div className={search ? 'opened' : 'closed'}>
                 <div className={'row justify-content-between mt-5'}>
                     <div className={'col-auto search-bar'}>
-                        <div className={'alphabets search'} role={'button'} tabIndex={0} onClick={() => toggleSearch(true )} onKeyDown={() => toggleSearch(true )}>
+                        <div className={'alphabets search'} role={'button'} tabIndex={0} onClick={() => {toggleSearch(true );toggleFilter(true );}} onKeyDown={() => {toggleSearch(true );toggleFilter(true );}}>
                             <ul className={'list-group list-group-horizontal'}>
                                 <li className={'list-group-item px-0'}>
                                     <input type="text" className={'form-control float-start'} onKeyUp={(e) => setGlossaries(SearchGlossary(e.target.value))}/>
@@ -69,11 +89,14 @@ function GetGlossaries(props){
                         <div className={'col-inner'}>
                             <div className={'alphabets float-end'}>
                                 <ul className={'list-group list-group-horizontal'}>
-                                    <li className={'list-group-item expand'} role={'button'} tabIndex={0} onClick={() => toggleSearch(false)} onKeyDown={() => toggleSearch(false )}><i className={'icon-dropdown'} aria-label={'Hide search'}></i></li>
+                                    <li className={'list-group-item expand'} role={'button'} tabIndex={0} onClick={() => {toggleSearch(false );toggleFilter(true );}} onKeyDown={() => {toggleSearch(false );toggleFilter(true );}}><i className={'icon-dropdown'} aria-label={'Hide search'}></i></li>
+                                    <li className={(!filter && props.alpha == 'all') ? 'list-group-item flex-fill active' : 'list-group-item flex-fill'} key={'all'}>
+                                        <Link to={`/glossary`} state={{ alpha: 'all' }} onClick={() => {toggleSearch(false ); toggleFilter(false)}}>{'ALL'}</Link>
+                                    </li>
                                     {glossaries.map((glossary,index) => {
                                             const alpha = String(Object.keys(glossary))
-                                            return (<li className={(!search && !props.alpha && index == 0) ? 'list-group-item flex-fill active' : props.alpha === alpha && !search ? 'list-group-item flex-fill active' : 'list-group-item flex-fill'} key={alpha}>
-                                                <Link to={`/glossary#${alpha}`} state={{ alpha: alpha }} onClick={() => toggleSearch(false )}>{alpha}</Link>
+                                            return (<li className={(!search && filter && !props.alpha && index == 0) ? 'list-group-item flex-fill active' : props.alpha === alpha && filter && !search ? 'list-group-item flex-fill active' : 'list-group-item flex-fill'} key={alpha}>
+                                                <Link to={`/glossary#${alpha}`} state={{ alpha: alpha }} onClick={() => {toggleSearch(false ); toggleFilter(true)}}>{alpha}</Link>
                                             </li>)
                                         }
                                     )}
@@ -84,8 +107,13 @@ function GetGlossaries(props){
                     </div>
                 </div>
             </div>
+
             <div className={'results'}>
-                {search && filteredGlossaries ?
+                {!filter && filteredGlossaries ?
+                    glossaries.map((glossary,index) =>
+                            <Results index={index} glossary={glossary}/>
+                        ) :
+                search && filteredGlossaries ?
                     <div>
                         {filteredGlossaries.length ? filteredGlossaries.map((glossary,index) =>
                                 <div className={'row alpha-row'} key={Object.keys(glossary)}>
@@ -149,7 +177,7 @@ class GlossaryPage extends React.Component {
                         <div className={'container'}>
                             <h1 className={'main'}>Glossary</h1>
 
-                            <GetGlossaries alpha={(this.props.location.state && this.props.location.state.alpha) ? this.props.location.state.alpha : String(Object.keys(glossaries[0]))}/>
+                            <GetGlossaries alpha={(this.props.location.state && this.props.location.state.alpha) ? this.props.location.state.alpha : 'all'}/>
                         </div>
                     </main>
                 </div>
