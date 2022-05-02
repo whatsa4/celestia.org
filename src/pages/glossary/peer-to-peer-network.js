fd5f56c4930afd83ce0b9f37ca3f5f8e2ff5b72c
Alex Beckett <75361908+alexbeckettt@users.noreply.github.com>
2022-05-02 14:12:53 +0930

    Add files via upload

--- src/pages/glossary/peer-to-peer-network.js
+++ src/pages/glossary/peer-to-peer-network.js
@@ -12,7 +12,7 @@ class GlossaryContent extends React.Component {
     render() {
         return (
             <div className={'glossary-content'}>
-                <p>A network in which nodes are connected and can communicate directly with each other without reliance on a third party to facilitate communication. Peer-to-peer (also known as p2p networks) networks are used in blockchains as a way for nodes to connect and send data between each other, such as transactions and block headers, among other things.
+                <p>A network in which nodes are connected and can communicate directly with each other without reliance on a third party to facilitate communication. Peer-to-peer networks are used in blockchains as a way for nodes to connect and send data between each other, such as transactions and block headers, among other things.
                 </p>
 
 
