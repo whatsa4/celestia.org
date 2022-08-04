646a06fcefc158d98948eee8bdf4cc536676fca2
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2022-08-04 20:49:48 +0930

    Update dispute-resolution.js

--- src/pages/glossary/dispute-resolution.js
+++ src/pages/glossary/dispute-resolution.js
@@ -12,7 +12,7 @@ class GlossaryContent extends React.Component {
     render() {
         return (
             <div className={'glossary-content'}>
-                <p>The handling and resolution of disputes. Dispute resolution is a term commonly used in relation to optimistic rollups and their fraud proof mechanism. Optimistic rollups submit transactions to an L1 under the assumption they are correct. If an eligible entity disagrees with the validity of the rollup’s transactions, they can issue a dispute and generate a fraud proof.
+                <p>The handling and resolution of disputes, commonly used in relation to optimistic rollups and their fraud proof mechanism. Optimistic rollups submit transactions to an L1 under the assumption they are correct. If an eligible entity disagrees with the validity of the rollup’s transactions, they can issue a dispute and generate a fraud proof.
                 </p>
 
                 <p>The dispute is considered resolved once the fraud proof has been verified, any offending parties are punished, and any required changes of the rollup’s chain are complete. The dispute resolution is said to be external if the process occurs on the L1 instead of on the rollup.
