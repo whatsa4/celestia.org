1cffc62a52e379c90674e22f20d114b51371720f
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2022-08-07 21:51:17 +0930

    Update content.js

--- src/datas/home/content.js
+++ src/datas/home/content.js
@@ -8,7 +8,7 @@ export const content = {
 export const FooterBoxes = [
     {
         title: 'Experiment with testnet',
-        text: 'Join a growing ecosystem of developers building on Celestia\'s testnet.',
+        text: 'Join a growing ecosystem of developers building sovereign rollups, settlement layers, and Celestiums on the testnet.',
         button: {
             text: 'Get started',
             href: 'https://docs.celestia.org/',
