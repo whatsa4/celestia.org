ca5b3ffbee2cd3cb442aab0328e1a76ed17287bd
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2022-08-08 17:48:03 +0930

    Update content.js

--- src/datas/team/content.js
+++ src/datas/team/content.js
@@ -15,8 +15,8 @@ export const content = {
 
 export const FooterBoxes = [
     {
-        title: 'Join our expanding ecosystem',
-        text: 'Be part of our growing ecosystem of developers and innovators who are helping build the Celestia network.',
+        title: 'Experiment with testnet',
+        text: 'Join a growing ecosystem of developers building rollups and applications on the Mamaki testnet.',
         button: {
             text: 'Get started',
             href: 'https://celestia-intake.typeform.com/ecosystem#hidden1=xxxxx',
