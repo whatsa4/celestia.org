5636c8f8cdeb137daeee227ca65d36baeab61f95
dabit3 <dabit3@gmail.com>
2022-08-11 12:42:13 -0500

    added modular fellows banner

--- src/components/modules/banner.js
+++ src/components/modules/banner.js
@@ -12,13 +12,13 @@ export default class Banner extends React.Component {
         const self = this;
         document.getElementById("banner").classList.add("hide");
         setTimeout(function (){
-            localStorage.setItem('banner', 'true')
+            localStorage.setItem('modular-fellows-banner', 'true')
             self.setState({banner:false})
         },600);
     };
 
     componentDidMount() {
-        if (localStorage.getItem('banner')) {
+        if (localStorage.getItem('modular-fellows-banner')) {
             this.setState({banner:false})
         }
     }
@@ -29,15 +29,16 @@ export default class Banner extends React.Component {
             <div className={'container'}>
                 <div className={'close-banner'} onClick={this.closeBanner}><i className={'icon-close'}></i></div>
                 <div className={'row w-80 align-items-center justify-content-center'}>
-                    <div className={'col-2 position-relative d-none d-md-flex'}>
+                    {/* <div className={'col-2 position-relative d-none d-md-flex'}>
                         <Image alt={'Modular Summit'} filename={'modular-summit.svg'} />
-                    </div>
-                    <div className={'col-auto'}>Register for the Modular Summit!</div>
-                    <div className={'col-auto'}>
-                        <a href="https://modularsummit.dev" target={'_blank'} rel={'noreferrer'}>
-                            <button className={'button-simple sm'}>Register</button>
-                        </a>
-                    </div>
+                    </div> */}
+                    <div className={'col-auto'}>Modular Fellows applications are now open!</div>
+
+                    <a className={'col-auto'} href="https://blog.celestia.org/introducing-modular-fellows" target={'_blank'} rel={'noreferrer'}>
+                        <div>
+                            <button className={'button-simple sm'}>Learn more</button>
+                        </div>
+                    </a>
                 </div>
             </div>
         </div> : ''
