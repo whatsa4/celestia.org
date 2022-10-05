e33782ab8b6b6e336f474445d60244176092db06
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2022-10-05 22:54:48 +1030

    Update team-members.js

--- src/datas/team/team-members.js
+++ src/datas/team/team-members.js
@@ -234,19 +234,6 @@ export const teamMembers = [
             linkedin : 'https://www.linkedin.com/in/jeremysklaroff/',
         }
     },{
-        id : 21,
-        name : 'Nader Dabit',
-        title: 'Developer Relations',
-        text: 'Former DevRel at Edge & Node and former DevRel lead at AWS',
-        image: 'team/NaderDabit.png',
-        socials: {
-            twitter : 'https://twitter.com/dabit3',
-            discord : '',
-            github : 'https://github.com/dabit3',
-            email : '',
-            linkedin : '',
-         }
-    },{
         id : 22,
         name : 'Rootul Patel',
         title: 'Software Engineer',
