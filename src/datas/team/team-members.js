df5c8382626cdc298adba1242095d7a6d515e5d6
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2022-10-07 15:26:03 +1030

    Update src/datas/team/team-members.js
    Co-authored-by: joshcs.eth ᵍᵐ <46639943+jcstein@users.noreply.github.com>

--- src/datas/team/team-members.js
+++ src/datas/team/team-members.js
@@ -334,7 +334,7 @@ export const teamMembers = [
         socials: {
             twitter : 'https://twitter.com/JoshCStein',
             discord : '',
-            github : '',
+            github : 'https://github.com/jcstein',
             email : '',
             linkedin : '',
          }
