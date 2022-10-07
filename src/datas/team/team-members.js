44249bed0c5429bcb2ab99b271c919f908be157a
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2022-10-07 15:25:52 +1030

    Update src/datas/team/team-members.js
    Co-authored-by: joshcs.eth ᵍᵐ <46639943+jcstein@users.noreply.github.com>

--- src/datas/team/team-members.js
+++ src/datas/team/team-members.js
@@ -329,7 +329,7 @@ export const teamMembers = [
         id : 28,
         name : 'Josh Stein',
         title: 'Developer Relations',
-        text: 'Former DevRel at Ankr & former Teaching Assistant at buildspace',
+        text: 'Former DevRel at Ankr & Teaching Assistant at buildspace',
         image: 'team/JoshStein.png',
         socials: {
             twitter : '',
