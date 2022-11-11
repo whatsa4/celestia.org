78d194faa3c0881bb590e588b2e087028a8a3bc1
Gabor Sera <gabor@designatives.com>
2022-11-11 12:43:51 +0100

    change learn modular page bottom

--- src/components/modules/featured-learn.js
+++ src/components/modules/featured-learn.js
@@ -35,15 +35,21 @@ export default function FeaturedLearn(props) {
                                 if(edge.node.frontmatter.slug === props.current) {
                                     const prev = index === 0 ? data.allMarkdownRemark.edges.length - 1 : index-1;
                                     const next = index === data.allMarkdownRemark.edges.length - 1 ? 0 : index+1;
+                                    console.log(index);
 
                                     return (
                                         <div className={'row pb-5 learn-modules learn-modules-footer smaller'}>
-                                            <div className={'col col-12 col-md-6 pt-5 prev'}><LearnBox
-                                                key={data.allMarkdownRemark.edges[prev].node.id}
-                                                post={data.allMarkdownRemark.edges[prev].node}/></div>
-                                            <div className={'col col-12 col-md-6 pt-5 next'}><LearnBox
-                                                key={data.allMarkdownRemark.edges[next].node.id}
-                                                post={data.allMarkdownRemark.edges[next].node}/></div>
+                                            <div className={'col col-12 col-md-6 pt-5 prev'}>
+                                                <LearnBox
+                                                    type={'prev'}
+                                                    key={data.allMarkdownRemark.edges[prev].node.id}
+                                                    post={data.allMarkdownRemark.edges[prev].node}/>
+                                            </div>
+                                            <div className={'col col-12 col-md-6 pt-5 next'}>
+                                                <LearnBox
+                                                    type={'next'}
+                                                    key={data.allMarkdownRemark.edges[next].node.id}
+                                                    post={data.allMarkdownRemark.edges[next].node}/></div>
                                         </div>)
                                 }
                             })
