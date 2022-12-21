f4f8a74e71e5418df5b0642c5893d2ca714c6905
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2022-12-22 10:29:44 +1030

    Update data-availability.js

--- src/pages/glossary/data-availability.js
+++ src/pages/glossary/data-availability.js
@@ -16,7 +16,7 @@ class GlossaryContent extends React.Component {
                 <p>Data availability answers the question, has this data been published? Specifically, a node will verify data availability when it receives a new block that is getting added to the chain. The node will attempt to download all the transaction data for the new block to verify availability. If the node can download all the transaction data, then it successfully verified data availability, proving that the block data was actually published to the network.
                 </p>
                 
-                <img src={Image}/>
+                <img src={Image} style="width:1000px;height:500px;"/>
                 
                 <p> For an overview on data availability, read <a href="https://coinmarketcap.com/alexandria/article/what-is-data-availability">What is Data Availability?</a>
                 </p>
