149f80ce55395d1979e50cbe788cdf2aff3b53e9
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2022-12-21 13:09:11 +1030

    Update social-consensus.js

--- src/pages/glossary/social-consensus.js
+++ src/pages/glossary/social-consensus.js
@@ -12,7 +12,7 @@ class GlossaryContent extends React.Component {
     render() {
         return (
             <div className={'glossary-content'}>
-                <p>The process by which individuals come to an agreement on a change that will be made to a blockchain. Changes that require social consensus are typically outside the scope of the consensus protocol, such as governance-led upgrades or responses to attacks/network outages.
+                <p>Social consensus is a process where individuals come to agreement on a change that will be made to a blockchain. Changes that require social consensus are typically outside the scope of the consensus protocol, such as governance-led upgrades or responses to attacks and network outages.
                 </p>
 
             </div>
